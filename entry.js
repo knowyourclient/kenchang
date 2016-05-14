9aaa857ddd623aa8ca92429ba45324559ab71963
Ken Chang <kennethchang@Kenneths-MacBook-Air.local>
2016-05-14 00:34:41 -0700

    updating stuff

--- entry.js
+++ entry.js
@@ -1,8 +1,14 @@
-// main.js
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
-);
+var HelloWorld = React.createClass({
+  render: function(){
+    return (
+      <div>
+        Hello World!!
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
