899f576a65675ff3f3e2ad09cf3075caae8e7697
Ken Chang <kenneth@plethora.com>
2016-06-24 23:55:48 -0700

    adding another component

--- entry.js
+++ entry.js
@@ -1,23 +1,53 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Router = require('react-router').Router
+var Route = require('react-router').Route
+var Link = require('react-router').Link
 
 var divStyle = {
   fontFamily: 'Helvetica'
 };
 
-var HelloWorld = React.createClass({
+var App = React.createClass({
+  render: function(){
+    return (
+	  <Router>
+        <Route path='/' component={Hello} />
+        <Route path='/resume' component={Resume} /> 
+      </Router>)
+    }
+});
+
+var Hello = React.createClass({
   render: function(){
     return (
       <div>
 	    kenchang.org
 		<p>
+		  <a href="https://www.linkedin.com/in/kenchang1">linkedin</a><br/>
 		  <a href="https://www.facebook.com/kennethchang">facebook</a><br/>
 		  <a href="https://www.instagram.com/modestbook/">instagram</a><br/>
-		  <a href="https://www.linkedin.com/in/kenchang1">linkedin</a><br/>
+		</p>
+		<p>
+		  <Link to='/resume'>résumé</Link><br/>
+		  <a href="https://www.modestbook.com/">modestbook</a>
 		</p>
       </div>
     )
   }
 });
 
-ReactDOM.render(<div style={divStyle}><HelloWorld /></div>, document.getElementById('app'));
+var Resume = React.createClass({
+  render: function(){
+    return (
+      <div>
+	    <Link to='/'>kenchang.org</Link>
+		<p>
+		  try again later.
+		</p>
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(<div style={divStyle}><App /></div>, document.getElementById('app'));
